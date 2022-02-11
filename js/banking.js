7750162edfee1389b4192b02ef261fa1222de482
shakib-mia <smdshakibmia2001@gmail.com>
2022-02-11 19:14:58 +0600

    final commit

--- js/banking.js
+++ js/banking.js
@@ -1,23 +1,86 @@
-document.getElementById('deposit-btn').addEventListener('click', function () {
-      let depositInput = document.getElementById('deposit-input');
-      let depositAmount = parseFloat(depositInput.value);
-      let previousDepositTotal = document.getElementById('deposit-total');
-      let previousDepositTotalAmount = parseFloat(previousDepositTotal.innerText);
-      let previousBalanceTotal = document.getElementById('balance-total');
-      let previousBalanceTotalAmount = parseFloat(previousBalanceTotal.innerText);
+function getInputValue(inputId) {
+      let inputField = document.getElementById(inputId);
+      let inputAmountText = inputField.value;
+      let amountValue = parseFloat(inputAmountText);
 
-      previousDepositTotal.innerText = previousDepositTotalAmount + depositAmount;
-      previousBalanceTotal.innerText = previousBalanceTotalAmount + depositAmount;
+      inputField.value = "";
+      return amountValue;
+}
+
+function updateTotalField(totalFieldId, amount) {
+      const totalField = document.getElementById(totalFieldId);
+      const totalText = totalField.innerText;
+      const previousTotal = parseFloat(totalText);
+
+      totalField.innerText = previousTotal + amount;
+}
+
+function updateBalance(amount, isAdd) {
+      const balanceTotal = document.getElementById('balance-total');
+      const balanceTotalText = balanceTotal.innerText;
+      const previousBalanceTotal = parseFloat(balanceTotalText);
+
+      if (isAdd == true) {
+            balanceTotal.innerText = previousBalanceTotal + amount;
+      }
+      else {
+            balanceTotal.innerText = previousBalanceTotal - amount;
+      }
+}
+
+
+document.getElementById('deposit-btn').addEventListener('click', function () {
+      const depositAmount = getInputValue('deposit-input');
+
+      /* 
+
+      const depositTotal = document.getElementById('deposit-total');
+      const depositTotalText = depositTotal.innerText;
+      const previousDepositTotal = parseFloat(depositTotalText);
+
+      depositTotal.innerText = previousDepositTotal + depositAmount;
+      
+      */
+
+
+      updateTotalField('deposit-total', depositAmount);
+
+      /*  
+      const balanceTotal = document.getElementById('balance-total');
+       const balanceTotalText = balanceTotal.innerText;
+       const previousBalanceTotal = parseFloat(balanceTotalText);
+ 
+       balanceTotal.innerText = previousBalanceTotal + depositAmount;
+        */
+
+      updateBalance(depositAmount, true);
 })
 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-      let withdrawInput = document.getElementById('withdraw-input');
-      let withdrawAmount = parseFloat(withdrawInput.value);
-      let previousWithdrawTotal = document.getElementById('withdraw-total');
-      let previousWithdrawTotalAmount = parseFloat(previousWithdrawTotal.innerText);
-      let previousBalanceTotal = document.getElementById('balance-total');
-      let previousBalanceTotalAmount = parseFloat(previousBalanceTotal.innerText);
+      const withdrawAmount = getInputValue('withdraw-input');
 
-      previousWithdrawTotal.innerText = previousWithdrawTotalAmount + withdrawAmount;
-      previousBalanceTotal.innerText = previousBalanceTotalAmount - withdrawAmount;
+
+      /* 
+      
+      const withdrawTotal = document.getElementById('withdraw-total');
+      const previousWithdrawTotalText = withdrawTotal.innerText;
+      const previousWithdrawTotal = parseFloat(previousWithdrawTotalText); 
+      
+      withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount; 
+      
+      */
+
+      updateTotalField('withdraw-total', withdrawAmount);
+
+      /* 
+      
+      const balanceTotal = document.getElementById('balance-total');
+      const balanceTotalText = balanceTotal.innerText;
+      const previousBalanceTotal = parseFloat(balanceTotalText);
+
+      balanceTotal.innerText = previousBalanceTotal - withdrawAmount; 
+      
+      */
+
+      updateBalance(withdrawAmount, false)
 })
